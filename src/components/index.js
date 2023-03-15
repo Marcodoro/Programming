412a4dff4da85867cee906b64e1349a79869a726
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2023-03-15 21:14:53 +0100

    fixed the god damn error

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Footer } from './Footer';
-export { default as Nav } from './Nav';
-export { default as Sidebar } from './Sidebar';
-export { default as Homepage } from './Homepage';
+export { default as Footer } from './Footer/footer.jsx';
+export { default as Nav } from './Nav/nav.jsx';
+export { default as Sidebar } from './Sidebar/sidebar.jsx';
+export { default as Homepage } from './Homepage/homepage.jsx';
