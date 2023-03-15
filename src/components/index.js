322caa5e0daef66d70694b0eff635a3bdc5a6e67
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2023-03-15 21:05:46 +0100

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Footer } from './Footer';
+export { default as Nav } from './Nav';
+export { default as Sidebar } from './Sidebar';
+export { default as Homepage } from './Homepage';
